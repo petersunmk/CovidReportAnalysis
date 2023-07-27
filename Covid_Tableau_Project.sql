4e27dd3f64f14f9f835ad7ea3b1b64c3219e3b0d
maokun sun <90821383+petersunmk@users.noreply.github.com>
2023-07-26 19:42:23 -0700

    Update and rename Covid_Project.sql to Covid_Tableau_Project.sql
    add final Tahbeau dashboard  SQL

--- Covid_Tableau_Project.sql
+++ Covid_Tableau_Project.sql
@@ -1,3 +1,43 @@
+# Dashboard main contents #
+
+--Tableau Table 1
+---List Covid Cases, Deaths and its death'a ratio in the world
+SELECT SUM(new_cases) as TotalCases,SUM(new_deaths) as TotalDeaths,SUM(new_deaths) / SUM(new_cases) * 100 as DeathPercentage
+FROM CovidDeaths
+where continent is not null
+order by 1,2
+
+--Tableau Table 2
+---List total deaths by location 
+select location, SUM(cast(new_deaths as int)) AS TotalDeathCount from CovidDeaths
+where continent is null and location not in ('World', 'European Union', 'International')
+and location not like('%income')
+Group by location
+order by TotalDeathCount DESC
+
+--Tableau Table 3
+---Looking at countries with infection rates per population
+select Location,population,MAX(total_cases) AS HighestInfectedCount,(MAX(total_cases)/population)*100 as PercentagePopulationInfected from CovidDeaths
+where continent is not null
+Group by Location,population
+order by PercentagePopulationInfected DESC
+
+--Tableau Table 4
+-- Use CTE and add vaccinated ratio per country
+With VaccinatedPop(Continent,Location,Date,Population,New_Vaccination,VaccinatedPeople)
+as(
+Select dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations,
+SUM(CAST(vac.new_vaccinations AS bigint)) OVER (Partition by dea.location order by dea.location,dea.date) as TotalVaccinated
+from CovidDeaths dea
+Join CovidVaccines vac 
+on dea.location=vac.location and dea.date=vac.date
+where dea.continent is not null
+)
+select *, VaccinatedPeople/Population *100 as VaccinatedRatio from VaccinatedPop
+
+#--Other Data Analasis in SQL(SSMS)#
+
+--Show all tables 
 select * from CovidDeaths
 where continent is not null
 order by 3,4
@@ -18,7 +58,7 @@ where location like 'United States' and continent is not null
 order by 1,2
 
 -- Total Cases vs Population
- Shows the likelihood of population infected with Covid
+-- Shows the likelihood of population infected with Covid
 select Location,date,total_cases,population,(total_cases/population)*100 as PercentagePopulationInfected from CovidDeaths
 where location like 'United States' and continent is not null
 order by 1,2
@@ -37,11 +77,12 @@ Group by Location
 order by TotalDeathCount DESC
 
 --Group it by continents with the highest death count per population
-select continent, MAX(cast(total_deaths as int)) AS TotalDeathCount from CovidDeaths
+select continent, Max(cast(total_deaths as int)) AS TotalDeathCount from CovidDeaths
 where continent is not null
 Group by continent
 order by TotalDeathCount DESC
-Global DeathPercentage
+
+--Global DeathPercentage
 SELECT SUM(new_cases) as TotalCases,SUM(new_deaths) as TotalDeaths,SUM(new_deaths) / SUM(new_cases) * 100 as DeathPercentage
 FROM CovidDeaths
 where continent is not null
@@ -55,20 +96,6 @@ Join CovidVaccines vac
 on dea.location=vac.location and dea.date=vac.date
 where dea.continent is not null
 order by 2,3
-
--- Use CTE and add vaccinated ratio per country
-
-With VaccinatedPop(Continent,Location,Date,Population,New_Vaccination,VaccinatedPeople)
-as(
-Select dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations,
-SUM(CAST(vac.new_vaccinations AS bigint)) OVER (Partition by dea.location order by dea.location,dea.date) as TotalVaccinated
-from CovidDeaths dea
-Join CovidVaccines vac 
-on dea.location=vac.location and dea.date=vac.date
-where dea.continent is not null
-)
-select *, VaccinatedPeople/Population *100 as VaccinatedRatio from VaccinatedPop
-
 
 --Temp Table
 
